2049774b8e0270436efdbe73b511d74e3f9ce432
surajpatelcs <suraj326len@gmail.com>
2021-07-08 17:34:25 +0530

    add drills

---
+++ strings.py
@@ -0,0 +1,26 @@
+def last_3_characters(x):
+    pass
+
+
+def first_10_characters(x):
+    pass
+
+
+def chars_4_through_10(x):
+    pass
+
+
+def str_length(x):
+    pass
+
+
+def words(x):
+    pass
+
+
+def capitalize(x):
+    pass
+
+
+def to_uppercase(x):
+    pass
